d320c38736f3b05f15382703692b1c388ea426b8
claytonmarcelo <marcelolimadez@gmail.com>
2026-04-13 17:27:27 -0300

    fix: use direct path to firebase credentials

--- server/dist/config/firebase.js
+++ server/dist/config/firebase.js
@@ -6,15 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
-let serviceAccount;
-const secretPath = '/etc/secrets/firebase-credentials.json';
-if (fs_1.default.existsSync(secretPath)) {
-    serviceAccount = JSON.parse(fs_1.default.readFileSync(secretPath, 'utf8'));
-}
-else {
-    serviceAccount = require(path_1.default.resolve('firebase-credentials.json'));
-}
+const serviceAccount = require(path_1.default.join(__dirname, '../../firebase-credentials.json'));
 if (!firebase_admin_1.default.apps.length) {
     firebase_admin_1.default.initializeApp({
         credential: firebase_admin_1.default.credential.cert(serviceAccount),
--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,16 +1,7 @@
 import admin from 'firebase-admin'
 import path from 'path'
-import fs from 'fs'
 
-let serviceAccount: any
-
-const secretPath = '/etc/secrets/firebase-credentials.json'
-
-if (fs.existsSync(secretPath)) {
-  serviceAccount = JSON.parse(fs.readFileSync(secretPath, 'utf8'))
-} else {
-  serviceAccount = require(path.resolve('firebase-credentials.json'))
-}
+const serviceAccount = require(path.join(__dirname, '../../firebase-credentials.json'))
 
 if (!admin.apps.length) {
   admin.initializeApp({
