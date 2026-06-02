13e8d230c2576d532eb045a34089f97cd4f48949
claytonmarcelo <marcelolimadez@gmail.com>
2026-06-02 15:16:44 -0300

    Remove BOM do api.ts e ajusta .gitignore para permitir rastrear .env.example

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,4 +1,4 @@
-﻿import axios, { type InternalAxiosRequestConfig } from 'axios'
+import axios, { type InternalAxiosRequestConfig } from 'axios'
 import { Platform } from 'react-native'
 import * as SecureStore from 'expo-secure-store'
 
